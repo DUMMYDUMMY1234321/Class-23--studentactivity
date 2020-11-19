4e9ad7e668a3cc813428ec9a69293850b036ebc1
DUMMYDUMMY1234321 <72018380+DUMMYDUMMY1234321@users.noreply.github.com>
2020-11-19 11:50:00 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World  = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+
+var ground;
+var box1, box2;
+
+function setup()
+{
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var gnd = 
+  {
+    isStatic : true
+  }
+  ground = Bodies.rectangle(200,390,400,20, gnd);
+  World.add(world, ground);
+
+ box1 = new Box(200,100,50,50);
+
+ box2 = new Box(100,100,50,50);
+}
+
+function draw() 
+{
+  background(0);  
+  Engine.update(engine);
+
+  fill("blueviolet");
+  rectMode(CENTER);
+  rect(ground.position.x , ground.position.y ,400,20);
+
+  box1.display();
+  box2.display();
+}
+
