2d0fc08f77617a3193f18b3492aae74500def323
DUMMYDUMMY1234321 <72018380+DUMMYDUMMY1234321@users.noreply.github.com>
2020-11-22 23:20:30 +0530

    Update Box.js

--- p5.play-boilerplate-master/Box.js
+++ p5.play-boilerplate-master/Box.js
@@ -10,6 +10,8 @@ class Box
         }
 
         this.body = Bodies.rectangle(x,y,width,height,option);
+        this.width = width;
+        this.height = height;
         World.add(world,this.body);
 
     }
